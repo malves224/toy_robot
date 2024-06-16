c9832b41670ba748d410ee4921b6e978c2d81254
Matheus Alves <malves224@gmail.com>
2024-06-16 17:53:38 -0300

    Refactor(tcp_server_game) use of commander

--- lib/tcp_server_game.rb
+++ lib/tcp_server_game.rb
@@ -1,12 +1,10 @@
-# lib/tcp_server.rb
-
 require 'socket'
-require File.expand_path('../config/environment', __dir__)
 
 class TcpServerGame
   def initialize(port)
     @server = TCPServer.new(port)
     @board = Board.new(size: 5)
+    @commander = CommandProcessorService.new(@board)
     puts "Server is running on port #{port}..."
   end
 
@@ -21,51 +19,17 @@ class TcpServerGame
     puts "Client connected: #{client.peeraddr.inspect}"
     client.puts 'Hello! You are connected to the server.'
     robo = nil
+    id = "#{client.peeraddr[2]}:#{client.peeraddr[1]}"
 
     while (message = client.gets)
-      puts client.peeraddr.inspect
-      puts "#{message}"
-      response = process_message(generate_connection_id(client), message.strip, robo)
-      client.puts response
+      output = @commander.call(id, message.strip, robo).data
+      client.puts output
 
-      robo = Robo.get(generate_connection_id(client)) if response.is_a?(Hash) && robo.nil?
+      robo = Robo.get(id) if robo.nil?
     end
 
     @board.remove(robo.id) if robo
     client.close
     puts 'Client disconnected.'
-  end
-
-  private
-
-  def process_message(connection_id, message, robo)
-    return place_robot(connection_id, message) if message.start_with?('PLACE')
-
-    return 'Robot is not on the table. Use the PLACE command.' if robo.nil?
-
-    execute_action(message, robo)
-  end
-
-  def place_robot(connection_id, message)
-    params = message.split(' ')[1..]
-    x = params[0].to_i
-    y = params[1].to_i
-    f = params[2]
-    robo = @board.place(connection_id, x, y, f)
-    robo ? robo.to_hash : 'Failed to place robot on the board.'
-  end
-
-  def execute_action(message, robo)
-    action = message.downcase.to_sym
-    if @board.respond_to?(action)
-      @board.send(action, robo)
-      robo.to_hash
-    else
-      'Unknown command.'
-    end
-  end
-
-  def generate_connection_id(client)
-    "#{client.peeraddr[2]}:#{client.peeraddr[1]}"
   end
 end
