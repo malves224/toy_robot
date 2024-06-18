692f5cfe2883e6589bb4e13d1ac7a96733448b48
Matheus Alves <malves224@gmail.com>
2024-06-18 00:05:39 -0300

    refactor(RedisApplication) instance redis

--- app/models/redis_application.rb
+++ app/models/redis_application.rb
@@ -1,6 +1,6 @@
 class RedisApplication
   def initialize
-    @redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
+    @redis = self.class.redis_instance
   end
 
   def create(attrs)
@@ -16,7 +16,24 @@ class RedisApplication
   end
 
   def self.get(key)
-    redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
-    redis.hgetall(key).transform_keys(&:to_sym)
+    redis_instance.hgetall(key).transform_keys(&:to_sym)
+  end
+
+  def self.all
+    hash_data = {}
+
+    redis_instance.scan_each do |key|
+      if redis_instance.type(key) == 'hash'
+        hash_data[key] = redis_instance.hgetall(key)
+      end
+    end
+
+    hash_data
+  end
+
+  private
+
+  def self.redis_instance
+    @redis ||= Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/1')
   end
 end
